040e318e837ee8e9ea33c59d5de9825e82e80aab
Maksym Davydov <mdavydov@adva-soft.com>
2022-01-01 23:18:59 +0200

    move AbstractData to algotest_data_holder.h

--- mathutils/algotest_tensor.h
+++ mathutils/algotest_tensor.h
@@ -20,71 +20,8 @@
 
 #include <algorithm>
 //#include <concepts>
-
+#include "algotest_data_holder.h"
 #include "algotest_tensor_impl.h"
-
-/**
- @brief const_holder class is used to implement const semantics for returned objects
- */
-template<class T>
-class const_holder
-{
-    T m_t;
-public:
-    const_holder(T&& t) : m_t(std::move(t)) {}
-    operator const T&() const { return m_t; }
-    const T* operator->() const { return &m_t; }
-};
-
-/// @brief initializer_t is a helper class for implicit initialization arguments
-template<class T> class initializer_t : public const_holder<T>
-{
-    public: initializer_t(T&& t) : const_holder<T>(std::move(t)) {}
-};
-
-template<class T> auto initializer(T&& val) { return initializer_t<T>(std::move(val)); }
-
-
-/// Abstract data holder
-class AbstractData
-{
-public:
-    virtual ~AbstractData() {}
-};
-
-template<class T>
-class ArrayPtr
-{
-    T* ptr;
-public:
-    ArrayPtr(T* ptr) : ptr(ptr) {}
-    ArrayPtr(ArrayPtr&& o) : ptr(o.ptr) { o.ptr = 0; }
-    ArrayPtr(const ArrayPtr& o)=delete;
-    ArrayPtr& operator=(const ArrayPtr& o)=delete;
-    ~ArrayPtr() { delete[] ptr; }
-};
-
-template<class T>
-class TypedData : public AbstractData
-{
-    T m_data;
-public:
-    TypedData(const T& data) : m_data(data) {}
-    TypedData(T&& data) : m_data( std::move(data) ) {}
-};
-
-template<class T>
-std::shared_ptr<AbstractData> abstractDataHolder(const T& data)
-{
-    return std::make_shared<TypedData<T>>(data);
-}
-
-template<class T>
-std::shared_ptr<AbstractData> abstractDataHolder(T&& data)
-{
-    return std::make_shared<TypedData<T>>( std::move(data) );
-}
-
 
 namespace algotest
 {
